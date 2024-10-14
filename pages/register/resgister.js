f4123d200e2d42b74c768b47ea970083e7449f8f
Dev-Lucas-Dias <diaslucasaparecido@gmail.com>
2024-10-14 09:53:32 -0300

    register

--- pages/register/resgister.js
+++ pages/register/resgister.js
@@ -1,18 +1,43 @@
-function validateEm(){
+/*function validateEm(){
     mostraBotao();
     erroEmail();
+}*/
+function validateEm(){
+    const email=all.email().value;
+    all.emailError().style.display=email ? "none" : "block";
+
+    all.emailInvalid().style.display=validateEmail(email) ? "none" : "block";
 }
 
+function senha(){
+    const senha =form.senha().value;
+    form.senhaNecess().style.display =senha ? "none" : "block";
+    form.senhaInvalid().style.display =validSenha(senha) ? "none" : "block";
+}
+
+function validSenha(){
+    const senha= all.senha().value;
+    if(!senha){return false}
+    return true;
+}
+function validateEmail(email){
+    return/\S+@\S+\.\S+/.test(email);
+}
 const all = {
     email: () => document.getElementById("email"),
     senha: () => document.getElementById("senha"),
+    confirmSenha : () => document.getElementById("confirmar-senha"),
     emailError: () => document.getElementById("email-error"),
     emailInvalid: () => document.getElementById("email-invalid"),
     senhaInvalid : () => document.getElementById("senha-invalid"),
+    senhaNecess : () => document.getElementById("senha-necessaria"),
+    senhaIgual : () => document.getElementById("senha-igual"),
     login : () => document.getElementById("enter"),
-    senhaRecover : () => document.getElementById("recuperar-senha")}
+    register : () => document.getElementById("register"),
+    senhaObri : () => document.getElementById("senha-obri")}
 
-function erroEmail(){
+
+/*function erroEmail(){
     const email=all.email().value;
     if(!email){
         all.emailError().style.display="block";
@@ -32,4 +57,4 @@ function isEmailValido(){
 }
 function validateEmail(email){
     return/\S+@\S+\.\S+/.test(email);
-}
+}*/
