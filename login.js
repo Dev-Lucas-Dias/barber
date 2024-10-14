cd42e6d2a26f95b96cbc342fb9ccb17f05286a54
Dev-Lucas-Dias <diaslucasaparecido@gmail.com>
2024-10-14 14:42:23 -0300

    resgister ok

--- login.js
+++ login.js
@@ -50,20 +50,7 @@ function mostraBotao(){
 }
 
 
-function validSenha(){
-    const senha= all.senha().value;
-    if(!senha){return false}
-    return true;
-}
 
-function isEmailValido(){
-    const email= all.email().value;
-    if(!email){return false;}
-    return validateEmail(email);
-}
-function validateEmail(email){
-    return/\S+@\S+\.\S+/.test(email);
-}
 
 function login(){
     loading();
--- pages/register/resgister.js
+++ pages/register/resgister.js
@@ -2,27 +2,6 @@
     mostraBotao();
     erroEmail();
 }*/
-function validateEm(){
-    const email=all.email().value;
-    all.emailError().style.display=email ? "none" : "block";
-
-    all.emailInvalid().style.display=validateEmail(email) ? "none" : "block";
-}
-
-function senha(){
-    const senha =form.senha().value;
-    form.senhaNecess().style.display =senha ? "none" : "block";
-    form.senhaInvalid().style.display =validSenha(senha) ? "none" : "block";
-}
-
-function validSenha(){
-    const senha= all.senha().value;
-    if(!senha){return false}
-    return true;
-}
-function validateEmail(email){
-    return/\S+@\S+\.\S+/.test(email);
-}
 const all = {
     email: () => document.getElementById("email"),
     senha: () => document.getElementById("senha"),
@@ -37,24 +16,46 @@ const all = {
     senhaObri : () => document.getElementById("senha-obri")}
 
 
-/*function erroEmail(){
+function validateEm(){
     const email=all.email().value;
-    if(!email){
-        all.emailError().style.display="block";
-    }else{
-        all.emailError().style.display="none";
-    }
-    if(validateEmail(email)){
-        all.emailInvalid().style.display="none";
-    }else{
-        all.emailInvalid().style.display="block";
-    }
+    all.emailError().style.display=email ? "none" : "block";
+    all.emailInvalid().style.display=validateEmail(email) ? "none" : "block";
+    register()
 }
-function isEmailValido(){
-    const email= all.email().value;
-    if(!email){return false;}
-    return validateEmail(email);
+function validarSenha(){
+    const senha =all.senha().value;
+    all.senhaInvalid().style.display = senha.length>=6 ? "none" : "block";
+   all.senhaNecess().style.display = senha ?"none" : "block";
+   validateSenhaIgual();
+   register()
 }
-function validateEmail(email){
-    return/\S+@\S+\.\S+/.test(email);
-}*/
+function confirmarSenha(){
+    validateSenhaIgual();
+    register()
+}
+ function validateSenhaIgual(){
+    const senha = all.senha().value;
+    const confirmSenha = all.confirmSenha().value;
+    all.senhaIgual().style.display= senha==confirmSenha ? "none" : "block";
+ }
+
+ function register(){
+    all.register().disabled=allValid();
+ }
+function allValid(){
+    const email = all.email().value;
+    if(!email|| !validateEmail(email)){
+        return false;
+        
+    }  all.register().style.cursor ="pointer";
+    const senha = all.senha().value;
+    if(!senha||senha.length<6){
+    return false;
+    }  all.register().style.cursor ="pointer";
+    const confirmaSenha = all.confirmSenha();
+    if(senha !=confirmSenha){
+        return false;
+    }  all.register().style.cursor ="pointer";
+    return true;
+   
+}
